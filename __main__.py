98ee41e5a36f3b819b121617a9412cf8bbf3e0c1
ermoshin.andrey2 <ermoshin.andrey2@wb.ru>
2025-03-12 17:56:17 +0300

    better args

--- __main__.py
+++ __main__.py
@@ -2,14 +2,31 @@ from summary import Summary, SummaryType
 from argparse import ArgumentParser
 
 if __name__ == "__main__":
-    parser = ArgumentParser()
+    parser = ArgumentParser(
+        usage="python3 __main__.py -f <filename> -o <output> -t -y -c",
+        description="Generate a summary of the data in the file"
+    )
 
-    parser.add_argument("-f", "--filename", type=str, default="data.csv")
-    parser.add_argument("-o", "--output", type=str, default="summary.csv")
-    parser.add_argument("-t", "--threads", type=int, default=4)
-    parser.add_argument("--summary-type", type=SummaryType, default=SummaryType.BY_TEACHER)
+    parser.add_argument("-f", "--filename", type=str, default="data.csv", help="Path to the CSV file with the data")
+    parser.add_argument("-o", "--output", type=str, default="summary.csv", help="Path to the output CSV file")
+    parser.add_argument("--threads", type=int, default=4, help="Number of threads to use")
+
+    parser.add_argument("-t", "--teacher", action="store_true", help="Group by teacher")
+    parser.add_argument("-y", "--year", action="store_true", help="Group by year")
+    parser.add_argument("-c", "--course", action="store_true", help="Group by course")
 
     args = parser.parse_args()
 
-    summary = Summary(args.filename, args.summary_type, args.threads, args.output)
+    if not any([args.teacher, args.year, args.course]):
+        parser.error("At least one of the following arguments is required: -t, -y, -c")
+
+    summary_type = 0
+    if args.teacher:
+        summary_type |= SummaryType.BY_TEACHER
+    if args.year:
+        summary_type |= SummaryType.BY_YEAR
+    if args.course:
+        summary_type |= SummaryType.BY_COURSE
+
+    summary = Summary(args.filename, summary_type, args.threads, args.output)
     summary.summarize()
